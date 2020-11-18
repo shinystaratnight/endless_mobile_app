a5d73c7206c33cfabb88895fd51d6bac0e6bd81c
phokke <phokke@yahoo.com>
2020-11-18 03:26:07 +0100

    add piiprent configuration

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,15 @@
-const String companyId = 'ba3bd324-e8ec-4095-9c14-5ab702fa16c5';
+// Laviin
+// const String companyId = 'ba3bd324-e8ec-4095-9c14-5ab702fa16c5';
+// const String clientId = 'wCGDXWbZFxhULVjnPYMxXkujONUHYHJnlH3mNKNM';
+// const String apiUrl = 'https://api.r3sourcertest.com';
+// const String formId = 'cede0726-1646-4940-aadc-56b91403d110';
+
+// Piiprent
+const String companyId = 'bbeed8cd-1b64-4c88-b0ae-5723bdd44ac3';
 const String clientId = 'wCGDXWbZFxhULVjnPYMxXkujONUHYHJnlH3mNKNM';
-const String apiUrl = 'https://api.r3sourcertest.com';
-const String formId = 'cede0726-1646-4940-aadc-56b91403d110';
+const String apiUrl = 'https://api.r3sourcer.com';
+const String formId = '58558379-4d7d-410c-8dad-19ed7d30b72e';
+
 const int listLimit = 5;
 
 const Map<String, String> languageMap = {
