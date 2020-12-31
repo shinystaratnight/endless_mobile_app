79204b3a43559cd602e43b36575adbba78cd78a5
phokke <phokke@yahoo.com>
2020-12-31 08:59:01 +0100

    small changes

--- lib/screens/preview_screen.dart
+++ lib/screens/preview_screen.dart
@@ -24,6 +24,7 @@ class PreviewScreen extends StatelessWidget {
         height: double.maxFinite,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset('images/company_banner.png'),
             CircularProgressIndicator(),
